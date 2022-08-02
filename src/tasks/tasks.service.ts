ecd4406fd9d60abb7ed219d66180c86f78651dc4
KJ Sudarshan <kjsudi@gmail.com>
2022-08-02 16:12:10 +0530

    add: new task model and enable communication between controller and service

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -3,4 +3,10 @@ import { Injectable } from '@nestjs/common';
 /* Note: making any component Injectable converts it into a Singleton
 Hence, the TasksService component here one such */
 @Injectable()
-export class TasksService {}
+export class TasksService {
+  private tasks = [];
+
+  getAllTasks() {
+    return this.tasks;
+  }
+}
