e98f5fc162e83f75720dc7758f7eb9fc9ced9212
KJ Sudarshan <kjsudi@gmail.com>
2022-08-02 16:15:54 +0530

    update: add type safety to tasks

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,10 +1,18 @@
 import { Injectable } from '@nestjs/common';
+import { Task, TaskStatus } from './tasks.model';
 
 /* Note: making any component Injectable converts it into a Singleton
 Hence, the TasksService component here one such */
 @Injectable()
 export class TasksService {
-  private tasks = [];
+  private tasks: Array<Task> = [
+    {
+      id: '1',
+      title: 'Wash Car',
+      description: 'Apply polish and wash',
+      status: TaskStatus.OPEN,
+    },
+  ];
 
   getAllTasks() {
     return this.tasks;
