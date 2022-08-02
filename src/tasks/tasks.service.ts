cd223c6413d87f6af320e6dbea569cbd56af3eb5
KJ Sudarshan <kjsudi@gmail.com>
2022-08-02 14:07:22 +0530

    update: add comment

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,4 +1,6 @@
 import { Injectable } from '@nestjs/common';
 
+/* Note: making any component Injectable converts it into a Singleton
+Hence, the TasksService component here one such */
 @Injectable()
 export class TasksService {}
