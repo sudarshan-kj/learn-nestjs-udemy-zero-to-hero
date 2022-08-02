ed59867754e903bde1d8c199d5b0150f069aa626
KJ Sudarshan <kjsudi@gmail.com>
2022-08-02 16:27:04 +0530

    link: new handler to service

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post, Request } from '@nestjs/common';
 import { Task } from './tasks.model';
 import { TasksService } from './tasks.service';
 
@@ -9,5 +9,10 @@ export class TasksController {
   @Get()
   getAllTasks(): Task[] {
     return this.tasksService.getAllTasks();
+  }
+
+  @Post()
+  createTask() {
+    return this.tasksService.createTask('Sudarshan', 'Kj');
   }
 }
