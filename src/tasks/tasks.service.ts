27d03c18337cb63e4f10b1a98f06c9eea08d1809
KJ Sudarshan <kjsudi@gmail.com>
2022-08-02 16:25:00 +0530

    update: add create task handler

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,20 +1,26 @@
 import { Injectable } from '@nestjs/common';
 import { Task, TaskStatus } from './tasks.model';
+import { v4 as uuid } from 'uuid';
 
 /* Note: making any component Injectable converts it into a Singleton
 Hence, the TasksService component here one such */
 @Injectable()
 export class TasksService {
-  private tasks: Array<Task> = [
-    {
-      id: '1',
-      title: 'Wash Car',
-      description: 'Apply polish and wash',
-      status: TaskStatus.OPEN,
-    },
-  ];
+  private tasks: Task[] = [];
 
-  getAllTasks() {
+  getAllTasks(): Task[] {
     return this.tasks;
+  }
+
+  createTask(title: string, description: string): Task {
+    const task: Task = {
+      id: uuid(),
+      title,
+      description,
+      status: TaskStatus.OPEN,
+    };
+
+    this.tasks.push(task);
+    return task;
   }
 }
